1d06396cd9fb3a38d5c51fd54ad784724e2da44c
unknown <zintiamilagrosmeza@gmail.com>
2022-12-23 14:03:33 -0500

    codigo_base ocho

--- prob_2/sol_1.c
+++ prob_2/sol_1.c
@@ -1,39 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int num,i,sum=0;
     printf("---Numero abundante, perfecto o deficiente--");
+    int num,i,sum=0;
+    
+    
+    do
     {
         printf("\nIngrese numero positivo menor que 580:");
         scanf("%d",&num);
+        if(num > 580){
+            printf("\nNumero fallido!");
+        }
+        else
+            printf("\nNumero valido");
         
-        if(num < 580)
-        {
-            printf("Numero valido!");  
+    }
+    while(num > 580);
             //suma de divisores
-            for (i=1; i<num; i++){
-                if(num%i==0){
-                    sum = sum + i;
-                }
+        for (i=1; i<num; i++){
+            if(num%i==0){
+                sum = sum + i;
+        }
                 
             }
             //comparar 
-            if(sum > num)
-            printf("\nEs abundante");
-            else if(sum == num)
-            printf("\nEs Perfecto");
-            else
-            printf("\nEs deficiente");
-            return 0;
-        }
-        
-        else 
-        {
-            printf("Numero erroneo, vuelva a ingresar numero");
-        }
-        
-           
-    }    
+        if(sum > num)
+        printf("\nEs abundante");
+        else if(sum == num)
+        printf("\nEs Perfecto");
+        else
+        printf("\nEs deficiente");
+    
+    return 0;
     
   
 }
