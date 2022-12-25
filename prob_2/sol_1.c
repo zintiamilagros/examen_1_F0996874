2ad89f9c23d751f61efd9ba1c0f646ebcad4c84c
unknown <zintiamilagrosmeza@gmail.com>
2022-12-25 00:30:39 -0500

    Solucion_problema_2

--- prob_2/sol_1.c
+++ prob_2/sol_1.c
@@ -1,37 +1,49 @@
 #include<stdio.h>
+#include<stdbool.h>
 int main()
 {
-    printf("---Numero abundante, perfecto o deficiente--");
-    int num,i,sum=0;
-    
+  printf("---Numero abundante, perfecto o deficiente--");
+  int num, i, sum = 0;
+  bool aux = true;
+  while (aux) {
     do
     {
-        printf("\nIngrese numero positivo menor que 580:");
-        scanf("%d",&num);
-        if(num > 580){
-            printf("\nNumero fallido!");
-        }
-        else
-            printf("\nNumero valido");
-        
+      printf("\nIngrese numero positivo menor que 580: ");
+      scanf("%d", &num);
+      
+      if (num > 580) {
+        printf("Numero fallido!\n");
+      }
+      else if (num == 0) {
+        aux = false;
+        break;
+      } 
+      else {
+        printf("Numero valido!");
+      }
     }
-    while(num > 580);
-        //suma de divisores
-        for (i=1; i<num; i++){
-            if(num%i==0){
-                sum = sum + i;
+    while (num > 580);
+    if (!aux) break;
+    else {
+    
+    //suma de divisores
+    sum=0;
+      for (i = 1; i < num; i++) {
+        if (num % i == 0) {
+          sum = sum + i;
         }
-                
-            }
-        //comparar 
-        if(sum > num)
-        printf("\nEs abundante");
-        else if(sum == num)
-        printf("\nEs Perfecto");
-        else
-        printf("\nEs deficiente");
+      }
     
-    return 0;
-    
-  
+    //comparar
+      if (sum > num)
+        printf("\nEs abundante\n");
+      else if (sum == num)
+        printf("\nEs Perfecto\n");
+      else
+        printf("\nEs deficiente\n");
+    }
+
+  }
+  printf("\nEso es todo amigos");
+  return 0;
 }
